f23cc27150ffa92d28c56c7978e3ea5d8761c062
operator <loganx775@gmail.com>
2025-05-15 17:20:07 -0700

    Login page QOL

--- simply/app/index.tsx
+++ simply/app/index.tsx
@@ -1,11 +1,35 @@
-import { Text, View, StyleSheet, TextInput, KeyboardAvoidingView, Platform } from "react-native";
+import { Text, View, StyleSheet, TextInput, KeyboardAvoidingView, Platform, Button} from "react-native";
 
-import React from 'react'
+import React, {useRef, useEffect, useState} from 'react'
 export default function Index() {
   const [email, onChangeEmail] = React.useState();
   const [username, onChangeUsername] = React.useState(); 
-  const [password, onChangePassword] = React.useState(); 
-  return (
+  const [password, onChangePassword] = React.useState();
+  const [loginMode, setLoginMode] = React.useState(true);
+  const [authToken, setAuthToken] = React.useState("!NOTOKEN") // holds the auth token 
+  // for signing into the service 
+  const emailInputRef = React.createRef<TextInput>(null); 
+  const usernameInputRef = React.createRef<TextInput>(null); 
+  const passwordInputRef = React.createRef<TextInput>(null); // references for focusing each input dialog 
+  const switchLoginMode = () => {
+   // switches to login mode 
+   setLoginMode(true); 
+  }
+  const switchInputFromEmail = () => {
+    //Switches the input field from email, dependent on the state of the login page 
+     {!loginMode ? usernameInputRef.current?.focus() : passwordInputRef.current?.focus()}
+  }
+  const switchRegisterMode = () => {
+    // switches to register mode 
+    setLoginMode(false)
+  }
+  const sendRegisterRequest = () => {
+
+  }
+  const sendLoginRequest = () => {
+
+  }
+  if (authToken == "!NOTOKEN") return (
     <KeyboardAvoidingView
       behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
       style={{
@@ -20,22 +44,32 @@ export default function Index() {
     <TextInput
       style={styles.loginInput}
       value={email}
+      ref={emailInputRef}
       onChangeText={onChangeEmail}
+      onSubmitEditing={switchInputFromEmail}
       placeholder="e-mail">
       </TextInput>
-    <TextInput
+      {!loginMode ? <TextInput
       style={styles.loginInput}
       value={username}
+      ref={usernameInputRef}
       onChangeText={onChangeUsername}
+      onSubmitEditing={() => passwordInputRef.current?.focus()}
       placeholder="username">
-      </TextInput>
+      </TextInput> : null}
     <TextInput
       style={styles.loginInput}
       value={password}
+      ref={passwordInputRef}
       onChangeText={onChangePassword}
       placeholder="password"
+      onSubmitEditing({loginMode ? sendLoginRequest : sendRegisterRequest})
       secureTextEntry={true}>
       </TextInput>
+      <View style={styles.initButtonContainer}>
+      <Button title="Login" color={loginMode ? "black" : "grey"} onPress={switchLoginMode}/>
+      <Button title="Register" color={loginMode ? "grey" : "black"} onPress={switchRegisterMode}/>
+      </View> 
     </KeyboardAvoidingView>
 
   );
@@ -55,9 +89,10 @@ const styles = StyleSheet.create({
     padding: 10,
   },
   initButtonContainer: {
-    margin: 20, 
+    
+    margin: 100, 
     flexDirection : 'row',
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
   }
 
 }); 
